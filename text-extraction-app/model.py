d6065629dac9d73c09d08ab036cc9d99a9ee4b01
MajorTimberWolf <ak0929@srmist.edu.in>
2024-01-13 01:07:46 +0530

    fix: remove buggy hf-token

--- text-extraction-app/model.py
+++ text-extraction-app/model.py
@@ -4,7 +4,7 @@ import gradio as grad
 import os  # For creating interactive web interfaces (not used in this code snippet)
 
 # Load the Mistral-8x7B-Instruct-v0.1 model from Hugging Face Hub
-model = InferenceClient("mistralai/Mixtral-8x7B-Instruct-v0.1", token=os.environ['HF_TOKEN'])
+model = InferenceClient("mistralai/Mixtral-8x7B-Instruct-v0.1")
 
 # Define a function to format prompts for the model
 def prompter(message):
